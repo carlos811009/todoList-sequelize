20bfe7684fe52c6d0242485a17b7b18f39f707fe
carlos <carlos811009@gmail.com>
2021-06-10 21:51:43 +0800

    add login/register feature

--- app.js
+++ app.js
@@ -2,27 +2,40 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
-
+const session = require('express-session')
 const app = express()
 const PORT = 3000
-
 const db = require('./models')
 const User = db.User
 const Todo = db.Todo
 
+const passport = require('passport')
+const usePassport = require('./config/passport')
+
+
+
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
-
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+usePassport(app)
 
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
+
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
@@ -30,12 +43,44 @@ app.get('/users/register', (req, res) => {
 
 app.post('/users/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+    })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 })
 
 app.get('/users/logout', (req, res) => {
   res.send('logout')
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+app.get('/', (req, res) => {
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
 
 app.listen(PORT, () => {
